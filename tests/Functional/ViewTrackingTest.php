061b07a5b4c9a74f255b77fe91433b1eecb4b177
Amarjeet Singh Rai <jatt27@gmail.com>
2017-10-19 16:09:01 +0100

    Mock DynamoDbClient in tests

--- tests/Functional/ViewTrackingTest.php
+++ tests/Functional/ViewTrackingTest.php
@@ -3,12 +3,26 @@
 namespace App\Tests\Functional;
 
 use App\Service\Encryptor;
+use Aws\DynamoDb\DynamoDbClient;
 use Firebase\JWT\JWT;
+use Prophecy\Argument;
 use Symfony\Component\BrowserKit\Cookie as BKCookie;
 use Symfony\Component\HttpFoundation\Cookie;
 
 class ViewTrackingTest extends WebTestCase
 {
+    private $dynamoDbClientMock;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->dynamoDbClientMock = $this->prophesize(DynamoDbClient::class);
+        $this->dynamoDbClientMock->query(Argument::any())->willReturn(['Count' => 0]);
+        $this->dynamoDbClientMock->updateItem(Argument::any())->willReturn();
+        $this->dynamoDbClientMock->putItem(Argument::any())->willReturn();
+    }
+
     public function testInvalidRequestReturns400(): void
     {
         $content = [
@@ -17,8 +31,9 @@ class ViewTrackingTest extends WebTestCase
         ];
 
         $client = static::createClient();
-        $client->request('POST', '/', [], [], [], json_encode($content));
+        self::$kernel->getContainer()->set(DynamoDbClient::class, $this->dynamoDbClientMock->reveal());
 
+        $client->request('POST', '/', [], [], [], json_encode($content));
         $response = $client->getResponse();
 
         $this->assertEquals('Bad response', $response->getContent());
@@ -38,8 +53,9 @@ class ViewTrackingTest extends WebTestCase
         $auth = JWT::encode(['did' => $userId], base64_decode(getenv('PRIVATE_KEY')), 'RS256');
 
         $client = static::createClient();
-        $client->request('POST', '/', [], [], ['HTTP_AUTHORIZATION' => "Bearer {$auth}"], json_encode($content));
+        self::$kernel->getContainer()->set(DynamoDbClient::class, $this->dynamoDbClientMock->reveal());
 
+        $client->request('POST', '/', [], [], ['HTTP_AUTHORIZATION' => "Bearer {$auth}"], json_encode($content));
         $response = $client->getResponse();
 
         $this->assertEquals('Success', $response->getContent());
@@ -54,6 +70,8 @@ class ViewTrackingTest extends WebTestCase
         ];
 
         $client = static::createClient();
+        self::$kernel->getContainer()->set(DynamoDbClient::class, $this->dynamoDbClientMock->reveal());
+
         $client->request('POST', '/', [], [], [], json_encode($content));
 
         /** @var Cookie $cookie */
@@ -79,6 +97,8 @@ class ViewTrackingTest extends WebTestCase
         $userUuid = $encryptor->encrypt('test');
 
         $client = static::createClient();
+        self::$kernel->getContainer()->set(DynamoDbClient::class, $this->dynamoDbClientMock->reveal());
+
         $client->getCookieJar()->set(
             new BKCookie('userUuid', $userUuid, strtotime('+30 minutes'), '/', getenv('DOMAIN'), false)//TODO: false for the test, find a fix
         );
